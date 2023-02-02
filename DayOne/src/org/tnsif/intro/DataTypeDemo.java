1001b6b46e121dee277d65363199243dfdb8571e
Student <Student@IOE-AIDS-176>
2023-02-02 12:03:44 +0530

    dayone commit 0.1

--- DayOne/src/org/tnsif/intro/DataTypeDemo.java
+++ DayOne/src/org/tnsif/intro/DataTypeDemo.java
@@ -3,7 +3,27 @@ package org.tnsif.intro;
 public class DataTypeDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		byte a = 127;
+		int b = 127;
+		long b1 = 2444464;
+		float c = 64;
+		double d = 545.6;
+		
+		System.out.println(" Value of  is a " +a );
+		System.out.println(" Value of  is b" +b );
+
+		System.out.println(" Value of  is c" +c );
+
+		System.out.println(" Value of  is d" + d );
+		
+		int x = 'c';
+		
+		String Name = " Om";
+		
+		System.out.println("Name = "+ Name);
+		
+
 
 	}
 
