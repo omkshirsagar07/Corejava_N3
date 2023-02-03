1c77f2d21ba075de6f765b13f622c43898b86d57
Student <Student@IOE-AIDS-176>
2023-02-03 07:05:23 +0530

    commit switch daytwo

--- DayTwo/src/org/tnsif/decisionmaking/SwitchDemo.java
+++ DayTwo/src/org/tnsif/decisionmaking/SwitchDemo.java
@@ -1,10 +1,38 @@
 package org.tnsif.decisionmaking;
 
+import java.util.Scanner;
+
 public class SwitchDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Yor Choice");
+		int a = sc.nextInt();
+		switch(a){
+		
+		case 1: 
+			System.out.println("Johny Johny 🎶🎶");
+			break;
+		
+		case 2 :
+			System.out.println("Yes Pappa 😏😏");
+			break;
+		case 3 :
+			System.out.println("Eating Sugar 😏😏");
+			break;
+			
+		case 4 :
+			System.out.println("No Pappa 😒😒");
+			break;
+			
+		case 5 :
+			System.out.println("Open Your mouth 😛😛 ");
+			break;
+			
+		default:
+			System.out.println("HaHaHa 😁😁😁");
+		}
+		}
+	
 
 }
