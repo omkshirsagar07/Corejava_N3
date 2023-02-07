343e8523ae11a82f2a72e34b2ba18ce67817a074
Student <Student@IOE-AIDS-176>
2023-02-07 11:19:12 +0530

    assignmentFunctionrecurrsion Commit

---
+++ AssignmentFunctionRecurrsion/src/org/tnsif/recurrsion/FibbonacciUsingRecurrsion.java
@@ -0,0 +1,37 @@
+package org.tnsif.recurrsion;
+
+import java.util.Scanner;
+
+public class FibbonacciUsingRecurrsion {
+
+	static int fibbo(int n)
+	{
+		if(n==1)
+		{
+			return 1;
+		}
+		
+		if(n==0)
+		{
+			return 0;
+		}
+		
+		return fibbo(n-1)+fibbo(n-2);
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the limit for fibbo :");
+		
+		int n = sc.nextInt();
+		
+		System.out.println("fibbo  :"+ fibbo(n));
+		
+		sc.close();
+
+	}
+
+
+}
